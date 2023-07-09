b188e1956129c9add9546677b43ed5f32845594e
NO_ob <15161159+NO-ob@users.noreply.github.com>
2023-07-09 14:08:42 +0100

    update

--- lib/src/data/constants.dart
+++ lib/src/data/constants.dart
@@ -4,8 +4,8 @@ class Constants {
   static const String appName = "LoliSnatcher";
   // TODO don't forget to update on every new release
   // TODO take these from smth like .env?
-  static const String appVersion = "2.3.2";
-  static const int appBuildNumber = 182;
+  static const String appVersion = "2.3.3";
+  static const int appBuildNumber = 183;
   //
 
   static const int defaultItemLimit = 20;
