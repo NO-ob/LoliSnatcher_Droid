e0bb624f8a3a368f1baccb06dea7c79761b0016e
NaniDeagle <iipocto1deagle@gmail.com>
2025-05-29 21:18:06 +0300

    lint fix

--- lib/src/widgets/preview/waterfall_bottom_bar.dart
+++ lib/src/widgets/preview/waterfall_bottom_bar.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'dart:math';
 import 'dart:ui';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
