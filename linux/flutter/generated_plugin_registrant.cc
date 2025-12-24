f91008d72fd781c1d643355892a52581b2652843
NaniDeagle <iipocto1deagle@gmail.com>
2025-12-24 23:28:25 +0300

    refactoring

--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -14,7 +14,6 @@
 #include <media_kit_libs_linux/media_kit_libs_linux_plugin.h>
 #include <media_kit_video/media_kit_video_plugin.h>
 #include <url_launcher_linux/url_launcher_plugin.h>
-#include <volume_controller/volume_controller_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
   g_autoptr(FlPluginRegistrar) dynamic_color_registrar =
@@ -41,7 +40,4 @@ void fl_register_plugins(FlPluginRegistry* registry) {
   g_autoptr(FlPluginRegistrar) url_launcher_linux_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "UrlLauncherPlugin");
   url_launcher_plugin_register_with_registrar(url_launcher_linux_registrar);
-  g_autoptr(FlPluginRegistrar) volume_controller_registrar =
-      fl_plugin_registry_get_registrar_for_plugin(registry, "VolumeControllerPlugin");
-  volume_controller_plugin_register_with_registrar(volume_controller_registrar);
 }
