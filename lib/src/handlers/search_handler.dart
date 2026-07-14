bfbc9543e254edffc33db61201196c6e8f548b4c
NaniDeagle <iipocto1deagle@gmail.com>
2026-07-14 21:24:27 +0300

    better booru equality check for related tabs checks

--- lib/src/handlers/search_handler.dart
+++ lib/src/handlers/search_handler.dart
@@ -420,7 +420,7 @@ class SearchHandler {
 
     final onlyTagMatches = tabs.where((tab) => tab.tags.toLowerCase().trim() == tag);
     if (onlyTagMatches.isNotEmpty) {
-      if (onlyTagMatches.any((tab) => tab.selectedBooru.value == targetBooru)) {
+      if (onlyTagMatches.any((tab) => tab.selectedBooru.value.matchesIdentity(targetBooru))) {
         return HasTabWithTagResult.onlyTag;
       }
       return HasTabWithTagResult.onlyTagDifferentBooru;
@@ -439,7 +439,7 @@ class SearchHandler {
     for (int i = 0; i < tabs.length; i++) {
       final tab = tabs[i];
       final parts = tab.tags.toLowerCase().trim().split(' ');
-      if (parts.length == 1 && parts[0] == tag && tab.selectedBooru.value == currentBooru) {
+      if (parts.length == 1 && parts[0] == tag && tab.selectedBooru.value.matchesIdentity(currentBooru)) {
         result.add((i, tab));
       }
     }
@@ -452,7 +452,7 @@ class SearchHandler {
     for (int i = 0; i < tabs.length; i++) {
       final tab = tabs[i];
       final parts = tab.tags.toLowerCase().trim().split(' ');
-      if (parts.length == 1 && parts[0] == tag && tab.selectedBooru.value != currentBooru) {
+      if (parts.length == 1 && parts[0] == tag && !tab.selectedBooru.value.matchesIdentity(currentBooru)) {
         result.add((i, tab));
       }
     }
